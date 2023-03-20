9cbdf7821828a2858042e7f675f807ba729020ed
Daniela Nikolova <112074833+dnikolova7@users.noreply.github.com>
2023-03-20 11:17:34 +0200

    Final project
    My API key was disabled, so success page is not working.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require ("express");
 const bodyParser = require ("body-parser");
 const request = require ("request");
 const https = require ("https");
+const { dirname } = require("path");
 
 const app = express ();
 
@@ -37,14 +38,27 @@ app.post("/", function(req, res){
 
     const options = {
         method : "POST",
-        auth : "daniela1:891650aaad89c46c0a45009640e95a69-us6"  }
+        auth : "daniela1:891650aaad89c46c0a45009640e95a69-us6" 
+    }
 
     const request = https.request(url, options, function(response){
+       if (response.statusCode === 200){
+        res.sendFile (__dirname + "/success.html")
+       }
+       else {
+        res.sendFile(__dirname + "/failure.html")
+       }
         response.on("data", function(data){
             console.log(JSON.parse(data));
+            
     });
 
 }) ;
+
+app.post("/failure", function(req, res){
+    res.sendFile (__dirname + "/index.html");
+    // I can use this too - res.redirect("/");
+});
 
 request.write(jsonData);
 request.end();
